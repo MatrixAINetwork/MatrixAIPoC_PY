1d2dfc335b0689e9f01f215ba55807d0f10f7155
Sqlong <Sqlong@matrix.space>
2018-05-03 16:03:20 +0800

    merged my python codes into the folder Core

---
+++ core/src/election/SM2.py
@@ -0,0 +1,278 @@
+// Copyright 2018 The Matrix Authors
+// This file is part of the Matrix library.
+//
+// The Matrix library is free software: you can redistribute it and/or modify
+// it under the terms of the GNU Lesser General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// The Matrix library is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+// GNU Lesser General Public License for more details.
+//
+// You should have received a copy of the GNU Lesser General Public License
+// along with the Matrix library. If not, see <http://www.gnu.org/licenses/>.
+# SM2.py - The core part of the sm2 library
+# Author: zh
+# Modifier: zheng
+
+from random import choice
+import SM3
+
+sm2_N = int('FFFFFFFEFFFFFFFFFFFFFFFFFFFFFFFF7203DF6B21C6052B53BBF40939D54123', 16)
+sm2_P = int('FFFFFFFEFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFF00000000FFFFFFFFFFFFFFFF', 16)
+sm2_G = '32c4ae2c1f1981195f9904466a39c9948fe30bbff2660be1715a4589334c74c7bc3736a2f4f6779c59bdcee36b692153d0a9877cc62a474002df32e52139f0a0' 
+sm2_a = int('FFFFFFFEFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFF00000000FFFFFFFFFFFFFFFC',16)
+sm2_b = int('28E9FA9E9D9F5E344D5A9E4BCF6509A7F39789F515AB8F92DDBCBD414D940E93',16)
+sm2_a_3 = (sm2_a + 3) % sm2_P 
+Fp = 256
+
+def get_random_str(strlen):
+    letterlist = ['0','1','2','3','4','5','6','7','8','9','a','b','c','d','e','f']
+    str = ''
+    for i in range(strlen):
+        a = choice(letterlist)
+        str = '%s%s' % (str,a)
+    return str
+
+def kG(k, Point,len_para):
+    Point = '%s%s' % (Point, '1')
+    mask_str = '8'
+    for i in range(len_para-1):
+        mask_str += '0'
+    # print(mask_str)
+    mask = int(mask_str, 16)
+    Temp = Point
+    flag = False
+    for n in range(len_para * 4):
+        if (flag):
+            Temp = DoublePoint(Temp,len_para)
+        if (k & mask) != 0:
+            if (flag):
+                Temp = AddPoint(Temp, Point,len_para)
+            else:
+                flag = True
+                Temp = Point
+        k = k << 1
+    return ConvertJacb2Nor(Temp,len_para)
+
+def DoublePoint(Point,len_para): 
+    l = len(Point)
+    len_2 = 2 * len_para
+    if l< len_para*2:
+        return None
+    else:
+        x1 = int(Point[0:len_para], 16)
+        y1 = int(Point[len_para:len_2], 16)
+        if l == len_2:
+            z1 = 1
+        else:
+            z1 = int(Point[len_2:], 16)
+        T6 = (z1 * z1) % sm2_P
+        T2 = (y1 * y1) % sm2_P
+        T3 = (x1 + T6) % sm2_P
+        T4 = (x1 - T6) % sm2_P
+        T1 = (T3 * T4) % sm2_P
+        T3 = (y1 * z1) % sm2_P
+        T4 = (T2 * 8) % sm2_P
+        T5 = (x1 * T4) % sm2_P
+        T1 = (T1 * 3) % sm2_P
+        T6 = (T6 * T6) % sm2_P
+        T6 = (sm2_a_3 * T6) % sm2_P
+        T1 = (T1 + T6) % sm2_P
+        z3 = (T3 + T3) % sm2_P
+        T3 = (T1 * T1) % sm2_P
+        T2 = (T2 * T4) % sm2_P
+        x3 = (T3 - T5) % sm2_P
+
+        if (T5 % 2) == 1:
+            T4 = (T5 + ((T5 + sm2_P) >> 1) - T3) % sm2_P
+        else:
+            T4 = (T5 + (T5 >> 1) - T3) % sm2_P
+
+        T1 = (T1 * T4) % sm2_P
+        y3 = (T1 - T2) % sm2_P
+
+        form = '%%0%dx' % len_para
+        form = form * 3
+        return form % (x3, y3, z3)
+
+def AddPoint(P1, P2,len_para):  
+    len_2 = 2 * len_para
+    l1 = len(P1)
+    l2 = len(P2)
+    if (l1 < len_2) or (l2 < len_2):
+        return None
+    else:
+        X1 = int(P1[0:len_para], 16)
+        Y1 = int(P1[len_para:len_2], 16)
+        if (l1 == len_2):
+            Z1 = 1
+        else:
+            Z1 = int(P1[len_2:], 16)
+        x2 = int(P2[0:len_para], 16)
+        y2 = int(P2[len_para:len_2], 16)
+
+        T1 = (Z1 * Z1) % sm2_P
+        T2 = (y2 * Z1) % sm2_P
+        T3 = (x2 * T1) % sm2_P
+        T1 = (T1 * T2) % sm2_P
+        T2 = (T3 - X1) % sm2_P
+        T3 = (T3 + X1) % sm2_P
+        T4 = (T2 * T2) % sm2_P
+        T1 = (T1 - Y1) % sm2_P
+        Z3 = (Z1 * T2) % sm2_P
+        T2 = (T2 * T4) % sm2_P
+        T3 = (T3 * T4) % sm2_P
+        T5 = (T1 * T1) % sm2_P
+        T4 = (X1 * T4) % sm2_P
+        X3 = (T5 - T3) % sm2_P
+        T2 = (Y1 * T2) % sm2_P
+        T3 = (T4 - X3) % sm2_P
+        T1 = (T1 * T3) % sm2_P
+        Y3 = (T1 - T2) % sm2_P
+
+        form = '%%0%dx' % len_para
+        form = form * 3
+        return form % (X3, Y3, Z3)
+
+def ConvertJacb2Nor(Point,len_para): 
+    len_2 = 2 * len_para
+    x = int(Point[0:len_para], 16)
+    y = int(Point[len_para:len_2], 16)
+    z = int(Point[len_2:], 16)
+    # z_inv = Inverse(z, P)
+    z_inv = pow(z, sm2_P - 2, sm2_P)
+    z_invSquar = (z_inv * z_inv) % sm2_P
+    z_invQube = (z_invSquar * z_inv) % sm2_P
+    x_new = (x * z_invSquar) % sm2_P
+    y_new = (y * z_invQube) % sm2_P
+    z_new = (z * z_inv) % sm2_P
+    if z_new == 1:
+        form = '%%0%dx' % len_para
+        form = form * 2
+        return form % (x_new, y_new)
+    else:
+        print ("Point at infinity!!!!!!!!!!!!")
+        return None
+
+def Inverse(data, M,len_para): 
+    tempM = M - 2
+    mask_str = '8'
+    for i in range(len_para-1):
+        mask_str += '0'
+    mask = int(mask_str, 16)
+    tempA = 1
+    tempB = data
+
+    for i in range(len_para*4):
+        tempA = (tempA * tempA) % M
+        if (tempM & mask) != 0:
+            tempA = (tempA * tempB) % M
+        mask = mask >> 1
+
+    return tempA
+
+def Verify(Sign, E, PA,len_para):
+    r = int(Sign[0:len_para], 16)
+    s = int(Sign[len_para:2*len_para], 16)
+    e = int(E, 16)
+    t = (r + s) % sm2_N
+    if t == 0:
+        return 0
+
+    P1 = kG(s, sm2_G,len_para)
+    P2 = kG(t, PA,len_para)
+    # print(P1)
+    # print(P2)
+    if P1 == P2:
+        P1 = '%s%s' % (P1, 1)
+        P1 = DoublePoint(P1,len_para)
+    else:
+        P1 = '%s%s' % (P1, 1)
+        P1 = AddPoint(P1, P2,len_para)
+        P1 = ConvertJacb2Nor(P1,len_para)
+
+    x = int(P1[0:len_para], 16)
+    return (r == ((e + x) % sm2_N))
+
+def Sign(E, DA, K,len_para,Hexstr = 0): 
+    if Hexstr:
+        e = int(E, 16) 
+    else:
+        E = E.encode('utf-8')
+        E = E.hex() 
+        e = int(E, 16)
+
+    d = int(DA, 16)
+    k = int(K, 16)
+
+    P1 = kG(k, sm2_G,len_para)
+
+    x = int(P1[0:len_para], 16)
+    R = ((e + x) % sm2_N)
+    if R == 0 or R + k == sm2_N:
+        return None
+    d_1 = pow(d+1, sm2_N - 2, sm2_N)
+    S = (d_1*(k + R) - R) % sm2_N
+    if S == 0:
+        return None
+    else:
+        return '%064x%064x' % (R,S)
+
+def Encrypt(M,PA,len_para,Hexstr = 0):
+    if Hexstr:
+        msg = M 
+    else:
+        msg = M.encode('utf-8')
+        msg = msg.hex() 
+    k = get_random_str(len_para)
+    C1 = kG(int(k,16),sm2_G,len_para)
+    xy = kG(int(k,16),PA,len_para)
+    x2 = xy[0:len_para]
+    y2 = xy[len_para:2*len_para]
+    ml = len(msg)
+    t = SM3.KDF(xy,ml/2)
+    # print(t)
+    if int(t,16)==0:
+        return None
+    else:
+        form = '%%0%dx' % ml
+        C2 = form % (int(msg,16) ^ int(t,16))
+        # print('C2 = %s'% C2)
+        # print('%s%s%s'% (x2,msg,y2))
+        C3 = SM3.Hash_sm3('%s%s%s'% (x2,msg,y2),1)
+        # print('C3 = %s' % C3)
+        return '%s%s%s' % (C1,C3,C2)
+
+def Decrypt(C,DA,len_para):
+    len_2 = 2 * len_para
+    len_3 = len_2 + 64
+    C1 = C[0:len_2]
+    C3 = C[len_2:len_3]
+    C2 = C[len_3:]
+    xy = kG(int(DA,16),C1,len_para)
+    # print('xy = %s' % xy)
+    x2 = xy[0:len_para]
+    y2 = xy[len_para:len_2]
+    cl = len(C2)
+    # print(cl)
+    t = SM3.KDF(xy, cl/2)
+    # print('t = %s'%t)
+    if int(t,16) == 0:
+        return None
+    else:
+        form = '%%0%dx' % cl
+        M = form % (int(C2,16) ^ int(t,16))
+        # print('M = %s' % M)
+        u = SM3.Hash_sm3('%s%s%s'% (x2,M,y2),1)
+        if  (u == C3):
+            return M
+        else:
+            return None
+
+#add by zheng
+def GeneratePubkey(k):
+    len_para = 64
+    return kG(k, sm2_G,len_para)
---
+++ core/src/election/SM3.py
@@ -0,0 +1,224 @@
+// Copyright 2018 The Matrix Authors
+// This file is part of the Matrix library.
+//
+// The Matrix library is free software: you can redistribute it and/or modify
+// it under the terms of the GNU Lesser General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// The Matrix library is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+// GNU Lesser General Public License for more details.
+//
+// You should have received a copy of the GNU Lesser General Public License
+// along with the Matrix library. If not, see <http://www.gnu.org/licenses/>.
+# Author: zh
+# Modifier: zheng
+
+from  math import ceil
+
+IV="7380166f 4914b2b9 172442d7 da8a0600 a96f30bc 163138aa e38dee4d b0fb0e4e"
+IV = int(IV.replace(" ", ""), 16)
+a = []
+for i in range(0, 8):
+    a.append(0)
+    a[i] = (IV >> ((7 - i) * 32)) & 0xFFFFFFFF
+IV = a
+
+def rotate_left(a, k):
+    k = k % 32
+    return ((a << k) & 0xFFFFFFFF) | ((a & 0xFFFFFFFF) >> (32 - k))
+
+T_j = []
+for i in range(0, 16):
+    T_j.append(0)
+    T_j[i] = 0x79cc4519
+for i in range(16, 64):
+    T_j.append(0)
+    T_j[i] = 0x7a879d8a
+
+def FF_j(X, Y, Z, j):
+    if 0 <= j and j < 16:
+        ret = X ^ Y ^ Z
+    elif 16 <= j and j < 64:
+        ret = (X & Y) | (X & Z) | (Y & Z)
+    return ret
+
+def GG_j(X, Y, Z, j):
+    if 0 <= j and j < 16:
+        ret = X ^ Y ^ Z
+    elif 16 <= j and j < 64:
+        #ret = (X | Y) & ((2 ** 32 - 1 - X) | Z)
+        ret = (X & Y) | ((~ X) & Z)
+    return ret
+
+def P_0(X):
+    return X ^ (rotate_left(X, 9)) ^ (rotate_left(X, 17))
+
+def P_1(X):
+    return X ^ (rotate_left(X, 15)) ^ (rotate_left(X, 23))
+
+def CF(V_i, B_i):
+    W = []
+    for i in range(16):
+        weight = 0x1000000
+        data = 0
+        for k in range(i*4,(i+1)*4):
+            data = data + B_i[k]*weight
+            weight = int(weight/0x100)
+        W.append(data)
+
+    for j in range(16, 68):
+        W.append(0)
+        W[j] = P_1(W[j-16] ^ W[j-9] ^ (rotate_left(W[j-3], 15))) ^ (rotate_left(W[j-13], 7)) ^ W[j-6]
+        str1 = "%08x" % W[j]
+    W_1 = []
+    for j in range(0, 64):
+        W_1.append(0)
+        W_1[j] = W[j] ^ W[j+4]
+        str1 = "%08x" % W_1[j]
+
+    A, B, C, D, E, F, G, H = V_i
+
+    for j in range(0, 64):
+        SS1 = rotate_left(((rotate_left(A, 12)) + E + (rotate_left(T_j[j], j))) & 0xFFFFFFFF, 7)
+        SS2 = SS1 ^ (rotate_left(A, 12))
+        TT1 = (FF_j(A, B, C, j) + D + SS2 + W_1[j]) & 0xFFFFFFFF
+        TT2 = (GG_j(E, F, G, j) + H + SS1 + W[j]) & 0xFFFFFFFF
+        D = C
+        C = rotate_left(B, 9)
+        B = A
+        A = TT1
+        H = G
+        G = rotate_left(F, 19)
+        F = E
+        E = P_0(TT2)
+
+        A = A & 0xFFFFFFFF
+        B = B & 0xFFFFFFFF
+        C = C & 0xFFFFFFFF
+        D = D & 0xFFFFFFFF
+        E = E & 0xFFFFFFFF
+        F = F & 0xFFFFFFFF
+        G = G & 0xFFFFFFFF
+        H = H & 0xFFFFFFFF
+
+
+    V_i_1 = []
+    V_i_1.append(A ^ V_i[0])
+    V_i_1.append(B ^ V_i[1])
+    V_i_1.append(C ^ V_i[2])
+    V_i_1.append(D ^ V_i[3])
+    V_i_1.append(E ^ V_i[4])
+    V_i_1.append(F ^ V_i[5])
+    V_i_1.append(G ^ V_i[6])
+    V_i_1.append(H ^ V_i[7])
+    return V_i_1
+
+def hash_msg(msg):
+    # print(msg)
+    len1 = len(msg)
+    reserve1 = len1 % 64
+    msg.append(0x80)
+    reserve1 = reserve1 + 1
+    # 56-64, add 64 byte
+    range_end = 56
+    if reserve1 > range_end:
+        range_end = range_end + 64
+
+    for i in range(reserve1, range_end):
+        msg.append(0x00)
+
+    bit_length = (len1) * 8
+    bit_length_str = [bit_length % 0x100]
+    for i in range(7):
+        bit_length = int(bit_length / 0x100)
+        bit_length_str.append(bit_length % 0x100)
+    for i in range(8):
+        msg.append(bit_length_str[7-i])
+
+    # print(msg)
+
+    group_count = round(len(msg) / 64)
+
+    B = []
+    for i in range(0, group_count):
+        B.append(msg[i*64:(i+1)*64])
+
+    V = []
+    V.append(IV)
+    for i in range(0, group_count):
+        V.append(CF(V[i], B[i]))
+
+    y = V[i+1]
+    result = ""
+    for i in y:
+        result = '%s%08x' % (result, i)
+    return result
+
+def str2byte(msg): 
+    ml = len(msg)
+    msg_byte = []
+    msg_bytearray = msg.encode('utf-8')
+    for i in range(ml):
+        msg_byte.append(msg_bytearray[i])
+    return msg_byte
+
+def byte2str(msg): 
+    ml = len(msg)
+    str1 = b""
+    for i in range(ml):
+        str1 += b'%c' % msg[i]
+    return str1.decode('utf-8')
+
+def hex2byte(msg): 
+    ml = len(msg)
+    if ml % 2 != 0:
+        msg = '0'+ msg
+    ml = int(len(msg)/2)
+    msg_byte = []
+    for i in range(ml):
+        msg_byte.append(int(msg[i*2:i*2+2],16))
+    return msg_byte
+
+def byte2hex(msg): 
+    ml = len(msg)
+    hexstr = ""
+    for i in range(ml):
+        hexstr = hexstr + ('%02x'% msg[i])
+    return hexstr
+
+def Hash_sm3(msg,Hexstr = 0):
+    if(Hexstr):
+        msg_byte = hex2byte(msg)
+    else:
+        msg_byte = str2byte(msg)
+    return hash_msg(msg_byte)
+
+
+def KDF(Z,klen): 
+    klen = int(klen)
+    ct = 0x00000001
+    rcnt = ceil(klen/32)
+    Zin = hex2byte(Z)
+    Ha = ""
+    for i in range(rcnt):
+        msg = Zin  + hex2byte('%08x'% ct)
+        # print(msg)
+        Ha = Ha + hash_msg(msg)
+        # print(Ha)
+        ct += 1
+    return Ha[0: klen * 2]
+
+'''
+if __name__ == '__main__':
+    y = Hash_sm3("abc")
+    print(y)
+
+    # klen = 19
+    # print(KDF("57E7B63623FAE5F08CDA468E872A20AFA03DED41BF1403770E040DC83AF31A67991F2B01EBF9EFD8881F0A0493000603", klen))
+'''    
+
+
+
